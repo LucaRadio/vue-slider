1d50f36c4773f38f231f121a1db6ece063728ab6
LucaRadio <luca.1407@hotmail.it>
2022-11-22 15:52:06 +0100

    Complete Exercise

---
+++ js/script.js
@@ -0,0 +1,72 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            imageDisplayIndex: 0,
+            slides: [
+                {
+                    image: 'img/01.webp',
+                    title: 'Marvel\'s Spiderman Miles Morale',
+                    text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                }, {
+                    image: 'img/02.webp',
+                    title: 'Ratchet & Clank: Rift Apart',
+                    text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                }, {
+                    image: 'img/03.webp',
+                    title: 'Fortnite',
+                    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                }, {
+                    image: 'img/04.webp',
+                    title: 'Stray',
+                    text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                }, {
+                    image: 'img/05.webp',
+                    title: "Marvel's Avengers",
+                    text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+                }
+            ],
+        };
+    },
+    methods: {
+        // startTimer: setInterval(this.changeImage, 3000),
+        prev() {
+            this.changeImage()
+        },
+
+        next() {
+            this.changeImage()
+        },
+        thumbImgClick(i) {
+            this.imageDisplayIndex = i;
+        },
+        changeImage(decrement) {
+            if (decrement === true) {
+                if (this.imageDisplayIndex === 0) {
+                    this.imageDisplayIndex = this.slides.length;
+                }
+                this.imageDisplayIndex--;
+
+            } else {
+                this.imageDisplayIndex++;
+                if (this.imageDisplayIndex === this.slides.length) {
+                    this.imageDisplayIndex = 0
+                }
+            }
+        },
+        // startInterval: function () {
+        //     setInterval(this.changeImage, 3000)
+        // },
+        // stopInterval: clearInterval(this.startInterval)
+    },
+    mounted: {
+        // start: this.startInterval(),
+
+    }
+}
+).mount("#app");
+
+
+
+
